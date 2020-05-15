6015eb2e6f57ebc6210534b3d1e6f3cea18cab16
lt-name <572413378@qq.com>
2020-05-15 18:42:58 +0800

    防止重复执行

--- src/main/java/cn/lanink/gunwar/tasks/VictoryTask.java
+++ src/main/java/cn/lanink/gunwar/tasks/VictoryTask.java
@@ -5,7 +5,6 @@ import cn.lanink.gunwar.room.Room;
 import cn.lanink.gunwar.utils.Language;
 import cn.lanink.gunwar.utils.Tools;
 import cn.nukkit.Player;
-import cn.nukkit.Server;
 import cn.nukkit.command.ConsoleCommandSender;
 import cn.nukkit.scheduler.PluginTask;
 
@@ -15,6 +14,7 @@ import java.util.Map;
 
 public class VictoryTask extends PluginTask<GunWar> {
 
+    private final String taskName = "VictoryTask";
     private final Language language = GunWar.getInstance().getLanguage();
     private final Room room;
     private int victoryTime;
@@ -39,26 +39,32 @@ public class VictoryTask extends PluginTask<GunWar> {
             this.cancel();
         }
         if (this.victoryTime < 1) {
-            List<String> vCmds = owner.getConfig().getStringList("胜利执行命令");
-            List<String> dCmds = owner.getConfig().getStringList("失败执行命令");
-            for (Map.Entry<Player, Integer> entry : room.getPlayers().entrySet()) {
-                if (this.room.victory == 1) {
-                    entry.getKey().sendTitle(this.language.victoryRed, "", 10, 30, 20);
-                    if (entry.getValue() == 1 || entry.getValue() == 11) {
-                        this.cmd(entry.getKey(), vCmds);
-                    }else if (entry.getValue() == 2 || entry.getValue() == 12) {
-                        this.cmd(entry.getKey(), dCmds);
-                    }
-                }else if (this.room.victory == 2) {
-                    entry.getKey().sendTitle(this.language.victoryBlue, "", 10, 30, 20);
-                    if (entry.getValue() == 1 || entry.getValue() == 11) {
-                        this.cmd(entry.getKey(), dCmds);
-                    }else if (entry.getValue() == 2 || entry.getValue() == 12) {
-                        this.cmd(entry.getKey(), vCmds);
+            if (!this.room.task.contains(this.taskName)) {
+                this.room.task.add(this.taskName);
+                if (this.room.getPlayers().size() > 0) {
+                    List<String> vCmds = owner.getConfig().getStringList("胜利执行命令");
+                    List<String> dCmds = owner.getConfig().getStringList("失败执行命令");
+                    for (Map.Entry<Player, Integer> entry : room.getPlayers().entrySet()) {
+                        if (this.room.victory == 1) {
+                            entry.getKey().sendTitle(this.language.victoryRed, "", 10, 30, 20);
+                            if (entry.getValue() == 1 || entry.getValue() == 11) {
+                                this.cmd(entry.getKey(), vCmds);
+                            } else if (entry.getValue() == 2 || entry.getValue() == 12) {
+                                this.cmd(entry.getKey(), dCmds);
+                            }
+                        } else if (this.room.victory == 2) {
+                            entry.getKey().sendTitle(this.language.victoryBlue, "", 10, 30, 20);
+                            if (entry.getValue() == 1 || entry.getValue() == 11) {
+                                this.cmd(entry.getKey(), dCmds);
+                            } else if (entry.getValue() == 2 || entry.getValue() == 12) {
+                                this.cmd(entry.getKey(), vCmds);
+                            }
+                        }
                     }
                 }
+                this.room.task.remove(this.taskName);
+                this.room.endGame();
             }
-            this.room.endGame();
             this.cancel();
         }else {
             this.victoryTime--;
