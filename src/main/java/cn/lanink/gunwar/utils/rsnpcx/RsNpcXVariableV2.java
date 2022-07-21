bd107f38948977fb5ecbb972b0902514ba26e2af
Glorydark <61799945+glorydark@users.noreply.github.com>
2022-07-21 10:46:58 +0800

    Update RsNpcXVariableV2.java

--- src/main/java/cn/lanink/gunwar/utils/rsnpcx/RsNpcXVariableV2.java
+++ src/main/java/cn/lanink/gunwar/utils/rsnpcx/RsNpcXVariableV2.java
@@ -30,12 +30,8 @@ public class RsNpcXVariableV2 extends BaseVariableV2 {
         }
 
         HashMap<String, Integer> map1 = new HashMap<>();
-        for(Level level: Server.getInstance().getLevels().values()) {
-            for (BaseRoom room : GunWar.getInstance().getRooms().values()) {
-                if (room.getLevelName().equals(level.getName())) {
-                    map1.put(room.getLevelName(), map1.getOrDefault(room.getLevelName(), 0) + room.getPlayers().size());
-                }
-            }
+        for (BaseRoom room : GunWar.getInstance().getRooms().values()) {
+            map1.put(room.getLevelName(), map1.getOrDefault(room.getLevelName(), 0) + room.getPlayers().size());
         }
         for (Map.Entry<String, Integer> entry : map1.entrySet()) {
             this.addVariable("{GunWarLevelPlayerNumber" + entry.getKey() + "}", entry.getValue().toString());
