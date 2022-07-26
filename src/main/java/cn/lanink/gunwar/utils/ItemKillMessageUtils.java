da76f7249d3159f53a4a28fe8b5b9486d46699c6
LT_Name <572413378@qq.com>
2022-07-26 20:58:58 +0800

    尝试优化性能

--- src/main/java/cn/lanink/gunwar/utils/ItemKillMessageUtils.java
+++ src/main/java/cn/lanink/gunwar/utils/ItemKillMessageUtils.java
@@ -3,38 +3,56 @@ package cn.lanink.gunwar.utils;
 import cn.lanink.gunwar.GunWar;
 import cn.nukkit.item.Item;
 import cn.nukkit.utils.Config;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import lombok.Data;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author LT_Name
  */
 public class ItemKillMessageUtils {
 
-    private static final HashMap<Item, String> ITEM_KILL_MESSAGE = new HashMap<>();
+    private static final HashMap<ItemData, String> ITEM_KILL_MESSAGE = new HashMap<>();
+    private static final Cache<Item, ItemData> ITEMDATA_CACHE = CacheBuilder.newBuilder().expireAfterAccess(3, TimeUnit.MINUTES).build();
+
 
     public static void load() {
         GunWar.getInstance().saveResource("ItemKillMessage.yml", false);
         new Config(GunWar.getInstance().getDataFolder() + "/ItemKillMessage.yml", Config.YAML).getAll().forEach((key, value) -> {
             Item item = Item.fromString(key);
             if (item.getId() != Item.AIR) {
-                ITEM_KILL_MESSAGE.put(item, value.toString());
+                ITEM_KILL_MESSAGE.put(ItemData.of(item), value.toString());
             }
         });
     }
 
     public static String getKillMessage(Item item) {
-        //TODO 优化
-        //return ITEM_KILL_MESSAGE.get(item);
-        for (Map.Entry<Item, String> entry : ITEM_KILL_MESSAGE.entrySet()) {
-            if (entry.getKey().getId() == item.getId() && entry.getKey().getDamage() == item.getDamage()) {
-                if (entry.getKey().getName().equals(item.getName())) {
-                    return entry.getValue();
-                }
-            }
+        return ITEM_KILL_MESSAGE.get(ItemData.of(item));
+    }
+
+    @Data
+    public static class ItemData {
+        public int id;
+        public int damage;
+        public String name;
+
+        private ItemData(Item item) {
+            this.id = item.getId();
+            this.damage = item.getDamage();
+            this.name = item.getName();
         }
-        return null;
+
+        public static ItemData of(Item item) {
+            ItemData itemData = ITEMDATA_CACHE.getIfPresent(item);
+            if (itemData == null) {
+                itemData = new ItemData(item);
+                ITEMDATA_CACHE.put(item, itemData);
+            }
+            return itemData;
+        }
     }
 
 }
