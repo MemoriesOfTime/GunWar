9cbdf290e6f5e527fdc127ad894e68c5e59bafdd
LT_Name <572413378@qq.com>
2022-09-07 23:06:29 +0800

    添加FlagSkinType枚举，准备使用枚举区分

---
+++ src/main/java/cn/lanink/gunwar/utils/FlagSkinType.java
@@ -0,0 +1,29 @@
+package cn.lanink.gunwar.utils;
+
+/**
+ * @author LT_Name
+ */
+public enum FlagSkinType {
+
+    /**
+     * 小旗帜
+     */
+    FLAG,
+
+    /**
+     * 旗帜底座
+     */
+    FLAG_STAND,
+
+    /**
+     * 长旗杆
+     */
+    LONG_FLAGPOLE,
+
+    /**
+     * 旗帜头
+     */
+    FLAG_HEAD
+
+
+}
